6a2b28d2b004de7c21c40ca6f68b88a925c1ce79
Clayton Watts <cletusw@gmail.com>
2014-09-05 23:39:00 -0600

    Use sh-app

---
+++ app/elements/sh-app/sh-app.js
@@ -0,0 +1,60 @@
+(function () {
+  'use strict';
+
+  Polymer('sh-app', {
+
+    created: function() {
+      this.answers = [];
+      this.puzzles = {};
+    },
+
+    remove: function(event, detail, sender) {
+      var removed = this.answers.splice(sender.parentElement.templateInstance.model.index, 1);
+      delete this.puzzles[removed[0]];
+      this.$.puzzlesStore.save();
+    },
+
+    reposition: function(event) {
+      var source = event.target.parentElement.templateInstance.model.index;
+      var destination = event.target.value - 1;
+      console.log(source, destination);
+
+      var removed = this.answers.splice(source, 1);
+      this.answers.splice(destination, 0, removed[0]);
+      this.$.answersStore.save();
+    },
+
+    savePuzzle: function(event) {
+      // Tab
+      if (event.which === 9) {
+        if (this.$.form.checkValidity()) {
+          this.answers.push(this.currentAnswer);
+          this.puzzles[this.currentAnswer] = [
+            this.currentClue1,
+            this.currentClue2,
+            this.currentClue3
+          ];
+          this.$.puzzlesStore.save();
+          this.currentAnswer = this.currentClue1 = this.currentClue2 = this.currentClue3 = '';
+        }
+        
+        this.$.hidden.focus();
+      }
+    },
+
+    validateAnswer: function() {
+      if (Object.keys(this.puzzles).indexOf(this.currentAnswer) === -1) {
+        this.$.answer.setCustomValidity('');
+      }
+      else {
+        this.$.answer.setCustomValidity('Duplicate answer');
+      }
+    },
+
+    keys: function(object) {
+      return Object.keys(object);
+    }
+
+  });
+
+})();
--- app/elements/yo-greeting/yo-greeting.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  'use strict';
-
-  Polymer('yo-greeting', {
-    greeting: '\'Allo'
-  });
-
-})();
